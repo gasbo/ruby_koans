8f834b58e7f268c5494b60e4552158c53c6699c6
gasbo <gasbo.dev@gmail.com>
2020-02-09 20:54:19 +0100

    fixed exceptions in triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if a <= 0 or b <= 0 or c <= 0
+  raise TriangleError if -a >= b - c or b - c >= a or b + c <= a
+
   return :equilateral if a == b and a == c
   return :isosceles if a == b or b == c or c == a
   return :scalene
@@ -22,3 +25,5 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+private
