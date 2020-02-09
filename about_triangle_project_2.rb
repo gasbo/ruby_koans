b54e8bb5420a5d57e40640adfd190199e3b47468
gasbo <gasbo.dev@gmail.com>
2020-02-09 20:53:58 +0100

    added test cases to test_illegal_triangles_throw_exceptions

--- about_triangle_project_2.rb
+++ about_triangle_project_2.rb
@@ -11,6 +11,10 @@ class AboutTriangleProject2 < Neo::Koan
     assert_raise(TriangleError) do triangle(3, 4, -5) end
     assert_raise(TriangleError) do triangle(1, 1, 3) end
     assert_raise(TriangleError) do triangle(2, 4, 2) end
+    assert_raise(TriangleError) do triangle(3, 1, 1) end
+    assert_raise(TriangleError) do triangle(1, 3, 1) end
+    assert_raise(TriangleError) do triangle(4, 2, 2) end
+    assert_raise(TriangleError) do triangle(2, 2, 4) end
     # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
   end
 end
